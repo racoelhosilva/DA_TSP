aeb22f6df6e6c92537980553ed617511456ced95
racoelhosilva <racoelhosilva@gmail.com>
2024-05-08 15:34:51 +0100

    docs: documentation for the interface

--- include/Interface.h
+++ include/Interface.h
@@ -6,6 +6,9 @@
 #include <cmath>
 #include "Graph.h"
 
+/**
+ * @brief Structure that represents a statistics about a certain TSP algorithm
+ */
 struct Statistic {
     std::string algorithm;
     double result;
@@ -19,8 +22,8 @@ class Interface {
 public:
 
     /**
-     * @brief Starts the interface by loading the dataset and cleaning the output file
-     * @return True if the water supply network was loaded accordingly and the output file was created/resetted
+     * @brief Starts the interface by loading the dataset
+     * @return True if the graph was loaded accordingly
      */
     bool init();
 
@@ -45,7 +48,7 @@ public:
     void waitInput();
 
     /**
-     * @brief Prints the options parts in the menu interface along with the highlighted option
+     * @brief Prints the options parts in the menu interface, formatted for the dataset section, along with the highlighted option
      * @param options The vector of options to show in the menu
      * @param choice The option that is currently selected (highlighted)
      */
@@ -53,14 +56,14 @@ public:
 
 
     /**
-     * @brief Prints the options parts in the menu interface along with the highlighted option, but the second last option is not shown in yellow
+     * @brief Prints the options parts in the menu interface, formatted for the algorithm section, along with the highlighted option
      * @param options The vector of options to show in the menu
      * @param choice The option that is currently selected (highlighted)
      */
     void printAlgorithmOptions(const std::vector<std::string> &options, int choice);
 
     /**
-     * @brief Main menu to select which function should be processed
+     * @brief Main menu to select which TSP algorithm should be processed
      */
     void mainMenu();
 
@@ -70,15 +73,28 @@ public:
     void exitMenu();
 
     /**
-     * @brief Menu used to select which dataset should be loaded into the program
-     * @return True if the dataset was loaded correctly, False otherwise
+     * @brief Menu used to select which dataset category should be loaded into the program
      */
     void categoryMenu();
 
+    /**
+    * @brief Menu used to select which extra fully connected graph should be loaded into the program
+    */
     void extraFullyConnectedMenu();
+
+    /**
+    * @brief Menu used to select which real world graph should be loaded into the program
+    */
     void realWorldMenu();
+
+    /**
+    * @brief Menu used to select which toy graph should be loaded into the program
+    */
     void toyMenu();
 
+    /**
+    * @brief Displays ordered statistics about all the algorithms that have been run in the current session
+    */
     void statistics();
 
 private:
@@ -87,13 +103,6 @@ private:
     Graph* graph = nullptr;
 
     std::vector<Statistic> stats;
-
-    std::pair<double, double> backtrackResult = {NAN, NAN};
-    std::pair<double, double> heldKarpResult = {NAN, NAN};
-    std::pair<double, double> doubleMSTResult = {NAN, NAN};
-    std::pair<double, double> nearestNeighborResult = {NAN, NAN};
-    std::pair<double, double> christofidesResult = {NAN, NAN};
-    std::pair<double, double> realWorldResult = {NAN, NAN};
 };
 
 #endif //DA_TSP_INTERFACE_H
