6302f972f65e3ad9549deeb6a8563b2cc162514f
Process-ing <rbruno.p.o@gmail.com>
2024-05-11 14:25:26 +0100

    Write documentation for the Ufds class

--- include/Ufds.h
+++ include/Ufds.h
@@ -1,14 +1,40 @@
 #ifndef DA_TSP_UFDS_H
 #define DA_TSP_UFDS_H
 
-
 #include <vector>
 
 class Ufds {
 public:
+    /**
+     * @brief Constructor of the Ufds class
+     * @details Complexity: O(n)
+     * @param n The number of elements in the Ufds
+     */
     Ufds(int n);
+
+    /**
+     * @brief Finds the set where the item i belongs
+     * @details Complexity: O(α(n)) amortized, where α(n) is the inverse Ackermann function
+     * @param i Item to check
+     * @return The item's set number
+     */
     int findSet(int i);
+
+    /**
+     * @brief Checks if two items are part of the same set
+     * @details Complexity: O(α(n)) amortized, where α(n) is the inverse Ackermann function
+     * @param i First item
+     * @param j Second item
+     * @return True if the items are part of the same set, and false otherwise
+     */
     bool isSameSet(int i, int j);
+
+    /**
+     * @brief Links the two sets where the items are stored
+     * @details Complexity: O(α(n)) amortized, where α(n) is the inverse Ackermann function
+     * @param i First item
+     * @param j Second item
+     */
     void linkSets(int i, int j);
 
 private:
